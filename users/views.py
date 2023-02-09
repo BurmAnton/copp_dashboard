b6d9237600c8a068439f8cd021986c8b3b70916b
BurmAnton <a.burmatsky@gmail.com>
2023-02-09 16:42:43 +0400

    Create and add to projects urls users.urls, add login&logout views

--- users/views.py
+++ users/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.contrib import auth
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@csrf_exempt
+def login(request):
+    message = None
+    if request.user.is_authenticated:
+        return HttpResponseRedirect(reverse("admin:index"))
+    elif request.method == "POST":
+        email = request.POST["email"]
+        password = request.POST["password"]
+
+        user = auth.authenticate(email=email, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return HttpResponseRedirect(reverse("login"))
+        else:
+            message = "Неверный логин и/или пароль."
+
+    return render(request, "users/login.html", {
+        "message": message,
+        "page_name": "ЦОПП СО | Авторизация"
+    })
+
+@login_required
+def logout(request):
+    auth.logout(request)
+    return HttpResponseRedirect(reverse("login"))
