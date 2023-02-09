53e1229b9c84cc1afb3e8cdc3c6a614f54f0fa33
BurmAnton <a.burmatsky@gmail.com>
2023-02-09 14:52:11 +0400

    Add reports models to admin.py

--- reports/admin.py
+++ reports/admin.py
@@ -1,3 +1,57 @@
 from django.contrib import admin
 
+from django_admin_listfilter_dropdown.filters import ChoiceDropdownFilter, \
+                                                     RelatedOnlyDropdownFilter
+
+from reports.models import ReportField, ReportTemplate, Tag, TimeInterval
+
 # Register your models here.
+@admin.register(ReportTemplate)
+class ReportTemplateypeAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+    )
+
+
+@admin.register(ReportField)
+class ReportFieldAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'item_number',
+        'report',
+        'field_type'
+    )
+    list_filter = (
+        ('field_type', ChoiceDropdownFilter),
+        ('report', RelatedOnlyDropdownFilter)
+    )
+
+    filter_horizontal = ("tags", "stop_tags")
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'tag_type'
+    )
+    list_filter = (
+        ('tag_type', ChoiceDropdownFilter),
+    )
+
+    filter_horizontal = ("programs", "group", "event")
+
+
+@admin.register(TimeInterval)
+class TimeIntervalAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'report',
+        'period',
+        'interval_type'
+    )
+    list_filter = (
+        ('period', ChoiceDropdownFilter),
+        ('interval_type', ChoiceDropdownFilter),
+        ('report', RelatedOnlyDropdownFilter)
+    )
